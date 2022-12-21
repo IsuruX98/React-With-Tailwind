99a3bc9261ac12c66aae81b4604af1c6752d7f23
IT21169380 <isurusanka98@gmail.com>
2022-12-21 12:30:37 +0530

    hero created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(true);
 
   const handleNav = () => {
     setNav(!nav);
@@ -25,10 +25,11 @@ const Navbar = () => {
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 ease-in-out duration-500"
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500 md:hidden"
             : "fixed left-[-100%]"
         }
       >
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-8">React.</h1>
         <ul className="uppercase p-4 mt-20">
           <li className="p-4 border-b border-gray-600">Home</li>
           <li className="p-4 border-b border-gray-600">Company</li>
